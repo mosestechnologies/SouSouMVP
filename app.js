93d1143b3646d57a5ca67aa3434c1d1a9b0922e3
Muhammad Sohaib Raza <sohaibraza000@gmail.com>
2020-08-13 04:54:35 +0500

    Controllers Added

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
-// Imports
-require('dotenv').config();
+/* --------------------------------- Imports -------------------------------- */
+require('dotenv').config({debug: true});
 const express = require('express');
-console.log("ENV: ", dotenv);
 //console.log(require('dotenv').config({debug: true}));
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const jwt = require('jsonwebtoken')
+
 
 // local imports
 const User = require('./modules/users/model/users');  // Users modle import
-//require('dotenv/config');
 
 //  Importing  routes
 const usersRoutes = require('./modules/users/routes/users');  // Users routes import
+const authRoute = require('./auth');
 const morgan = require('morgan');
 
-// Middleware
+/* ------------------------------- Middleware ------------------------------- */
+
 app.use(bodyParser.json());  // parse application/json
 app.use(bodyParser.urlencoded({ extended: true }))  // parse application/x-www-form-urlencoded
 app.use(morgan('combined'));  // Output to console the request routes + status
 
-    //  <<<<    ROUTES    >>>>
+
+/* --------------------------------- Routes --------------------------------- */
+
 app.use('/users', usersRoutes);  // Connecting to Products
+app.use('/api/auth', authRoute); // Connected to Authentication
+
 app.get("/", (req, res) => {
     res.json({'Success': 'Welcome home'})
 })
+
+
+/* -------------------------------- Database -------------------------------- */
 
 mongoose.connect(
 	process.env.CONNECT_DB,
@@ -36,7 +43,10 @@ mongoose.connect(
 	},
 	() => console.log("Database connected")
 );
-// server
-app.listen(5000, ()=>{
-    console.log("<<<  Sever Started  >>>");
+
+/* --------------------------------- server --------------------------------- */
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log(`<<<  Sever Started  >>> on port ${port}`);
 });
--- modules/users/routes/users.js
+++ modules/users/routes/users.js
@@ -1,56 +1,17 @@
 const express = require('express');
-const UsersModel = require('../model/users');
-const router = express.Router()
+const router = express.Router();
+const controller =  require('../controller/usersController');
 
 	// Add user route
-router.post('/', async (req,res) => {
-	console.log("Request: ", req.body);
-    try{
-		console.log("Request: ", req.body);
-
-		const data = await UsersModel.insertMany(req.body)
-		console.log("Data: ", data);
-		res.status(302).send("user successfully added");
-    }catch(err){
-		console.log("Error: ", err);
-		res.json({message : err});
-	}
-});
+router.post('/', controller.add);
 
 	// Find user route
-router.get('/find-user/:id', async (req,res) => {
-	console.log("Request: ", req.params.id);
-	try {
-		const user = await UsersModel.findOne()
-			.where("unique_id")
-			.equals(Number(req.params.id))
-		console.log(typeof(user));
-		if (user) {
-			res.json(user)
-		} else {
-			res.json({ unique_id: "No user found" })
-		}
-	} catch (error) {
-		res.json(error)
-	}
-});
+router.get('/find-user/:id', controller.find);
 
 	// Delete user route
-router.delete('/delete-user/:id', async (req, res)=>{
-	try {
-		const deleteUser = await UsersModel.deleteOne({unique_id : req.params.id})
-		res.json({deleted : "Item deleted Successfully"})
-	} catch (error) {
-		res.json(error)
-	}
-});
+router.delete('/delete-user/:id', controller.delete);
 
 	// Udpdate user route
-router.put('/update', async (req, res)=>{
-	try {
-	} catch (error) {
-		res.json(error)
-	}
-});
+router.put('/update', controller.update);
 
 module.exports =  router;
