9079e5eec1633fd6908422425f834464db24fbdf
tamjeedhur <tamjeedhur@gmail.com>
2020-09-26 17:44:41 +0500

    with homepage

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect, HashRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect,
+  HashRouter,
+} from "react-router-dom";
 import { AuthContext, reducer, initialState } from "./context/GlobalState";
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
-import { ProtectedRoute } from './ProtectedRoute';
-import Group from './views/Group';
-import JoinGroup from './views/JoinGroup';
+import Homepage from "layouts/Homepage.js";
+
+import { ProtectedRoute } from "./ProtectedRoute";
+import Group from "./views/Group";
+import JoinGroup from "./views/JoinGroup";
 
 /** <<<  @Styles  >>> */
 import "assets/plugins/nucleo/css/nucleo.css";
@@ -17,44 +25,49 @@ const App = () => {
   const [state, dispatch] = React.useReducer(reducer, initialState);
 
   useEffect(() => {
-    console.log( 'JSON PARSE: ', JSON.parse(localStorage.getItem('user') ));
-    const user = JSON.parse(localStorage.getItem('user') || null || state.user)
-    const token = (localStorage.getItem('auth-token') || null || state.token);
+    console.log("JSON PARSE: ", JSON.parse(localStorage.getItem("user")));
+    const user = JSON.parse(localStorage.getItem("user") || null || state.user);
+    const token = localStorage.getItem("auth-token") || null || state.token;
     // const user = state.user;
     // const token = state.token;
-    if(user && token){
+    if (user && token) {
       // console.log("USER: ", user, " TOKEN: ", token);
       dispatch({
-        type: 'LOGIN',
+        type: "LOGIN",
         payload: {
           user,
-          token
-        }
-      })
-
+          token,
+        },
+      });
     }
-  }, [state.token])
+  }, [state.token]);
   // console.log('Initial State', initialState);
 
-
   return (
-    <AuthContext.Provider value={ { state, dispatch } }>
+    <AuthContext.Provider value={{ state, dispatch }}>
       <HashRouter>
-      <BrowserRouter>
-        <Switch>
-          <ProtectedRoute path="/admin" render={ props => <AdminLayout {...props} /> } />
-          <Route path="/auth" render={ props => <AuthLayout {...props} /> } />
-          <Route path="/group/:groupId" render={ props => <Group {...props} /> } />
-          <ProtectedRoute path="/join/:userId/:groupId" render={ props => <JoinGroup {...props} /> } />
-          <Redirect from="/" to="/admin/index" />
-          <Route component={() => (<div>404 Not found </div>)} />
-        </Switch>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Switch>
+            <ProtectedRoute
+              path="/admin"
+              render={(props) => <AdminLayout {...props} />}
+            />
+            <Route path="/" render={(props) => <Homepage {...props} />} />
+            <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
+            <Route
+              path="/group/:groupId"
+              render={(props) => <Group {...props} />}
+            />
+            <ProtectedRoute
+              path="/join/:userId/:groupId"
+              render={(props) => <JoinGroup {...props} />}
+            />
+            <Redirect from="/" to="/admin/index" />
+            <Route component={() => <div>404 Not found </div>} />
+          </Switch>
+        </BrowserRouter>
       </HashRouter>
     </AuthContext.Provider>
-  )
+  );
 };
-ReactDOM.render(
-  <App/>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ client/src/layouts/Homepage.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+// reactstrap components
+import { Container, Row, Col } from "reactstrap";
+import "./Homepage.css";
+import { Button } from 'reactstrap';
+
+// core components
+import AuthNavbar from "components/Navbars/AuthNavbar.js";
+import AuthFooter from "components/Footers/AuthFooter.js";
+
+import routes from "routes.js";
+
+function Homepage() {
+  return (
+    <div class="homepage">
+      <div className="color_overlay"></div>
+      <AuthNavbar />
+      <div className="home__content">
+        <h1>SouSou</h1>
+        <span>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </span>
+        <Button className="button">Register Now</Button>
+      </div>
+
+       <AuthFooter class="home__footer" />
+    </div>
+  );
+}
+
+export default Homepage;
